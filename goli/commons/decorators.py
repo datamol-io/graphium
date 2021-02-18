bc6f88b060c27a816fd821fd069e37d19e0ce411
Dom <dominique@invivoai.com>
2021-02-18 12:35:02 -0500

    Minor change to classproperty docs.

--- goli/commons/decorators.py
+++ goli/commons/decorators.py
@@ -1,13 +1,13 @@
 class classproperty(property):
     r"""
-    Decorator used to declare a static property, defined for the class
+    Decorator used to declare a class property, defined for the class
     without needing to instanciate an object.
 
     !!! Example
 
         ``` python linenums="1"
             @classproperty
-            def my_static_property(cls):
+            def my_class_property(cls):
                 return 5
         ```
     """
