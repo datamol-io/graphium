1e0a881ce8a5aa78e30f25fdff75120ae07d4692
Hadrien Mary <hadrien.mary@gmail.com>
2021-03-16 17:41:22 -0400

    working refactored datamodule

--- goli/data/__init__.py
+++ goli/data/__init__.py
@@ -1,7 +1,8 @@
 from .utils import load_micro_zinc
+from .utils import load_tiny_zinc
+
+from .collate import goli_collate_fn
 
 from .dataset import BaseDataset
 from .dataset import SmilesDataset
-from .datamodule import DGLCollate
-from .datamodule import BaseDataModule
 from .datamodule import DGLFromSmilesDataModule
--- goli/data/utils.py
+++ goli/data/utils.py
@@ -3,9 +3,17 @@ import pandas as pd
 
 
 def load_micro_zinc() -> pd.DataFrame:
-    """Return a dataframe of micro ZINC."""
+    """Return a dataframe of micro ZINC (1000 data points)."""
 
     with importlib.resources.open_text("goli.data", "micro_ZINC.csv") as f:
-        df = pd.read_csv(importlib.resources.open_text("goli.data", "micro_ZINC.csv"))
+        df = pd.read_csv(f)
 
     return df
+
+def load_tiny_zinc() -> pd.DataFrame:
+    """Return a dataframe of tiny ZINC (100 data points)."""
+
+    with importlib.resources.open_text("goli.data", "micro_ZINC.csv") as f:
+        df = pd.read_csv(f)
+
+    return df.iloc[:100]
