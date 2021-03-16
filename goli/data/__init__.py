87e777bcd1c77aacf2ae1fb0a04746095d7027f4
Hadrien Mary <hadrien.mary@gmail.com>
2021-03-16 18:30:17 -0400

    clean merging

--- goli/data/__init__.py
+++ goli/data/__init__.py
@@ -3,6 +3,4 @@ from .utils import load_tiny_zinc
 
 from .collate import goli_collate_fn
 
-from .dataset import BaseDataset
-from .dataset import SmilesDataset
 from .datamodule import DGLFromSmilesDataModule
