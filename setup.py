578b0c88373add6f0ecee2084398d6038680d507
Hadrien Mary <hadrien.mary@gmail.com>
2021-03-24 16:09:55 -0400

    bumped version to 0.1.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name="goli",
-    version="0.0.1",
+    version="0.1.0",
     author="InVivo AI",
     author_email="dominique@invivoai.com",
     url="https://github.com/invivoai/goli",
