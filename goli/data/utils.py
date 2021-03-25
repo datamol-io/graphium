024dac37c438abbb64b4c5ccf760d8494804cc66
Dom <dominique@invivoai.com>
2021-03-25 09:47:49 -0400

    Updated Readme, dataset links, and jupyter examples

--- goli/data/utils.py
+++ goli/data/utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 def load_micro_zinc() -> pd.DataFrame:
     """Return a dataframe of micro ZINC (1000 data points)."""
 
-    with importlib.resources.open_text("goli.data", "micro_ZINC.csv") as f:
+    with importlib.resources.open_text("goli.data", "micro_ZINC/micro_ZINC.csv") as f:
         df = pd.read_csv(f)
 
     return df
@@ -14,7 +14,7 @@ def load_micro_zinc() -> pd.DataFrame:
 def load_tiny_zinc() -> pd.DataFrame:
     """Return a dataframe of tiny ZINC (100 data points)."""
 
-    with importlib.resources.open_text("goli.data", "micro_ZINC.csv") as f:
+    with importlib.resources.open_text("goli.data", "micro_ZINC/micro_ZINC.csv") as f:
         df = pd.read_csv(f, nrows=100)
 
     return df
