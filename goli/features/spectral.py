4186ed88b18a899fb5bed53c4d3cdcaa4f3736cd
Dominique <dominique@invivoai.com>
2021-04-15 12:20:40 -0400

    Tested and fixed the positional encoder

--- goli/features/spectral.py
+++ goli/features/spectral.py
@@ -1,7 +1,8 @@
 from typing import Optional, Tuple
 
 import scipy as sp
-import scipy.sparse.linalg as splinalg
+from scipy.sparse.linalg import eigs
+from scipy.linalg import eig
 from scipy.sparse import csr_matrix, diags, issparse, spmatrix
 import numpy as np
 import torch
@@ -39,8 +40,9 @@ def compute_laplacian_positional_eigvecs(
         for component in components:
             comp = list(component)
             this_L = L_norm[comp][:, comp]
-            _, this_eigvecs = _get_positional_eigvecs(this_L, num_pos=num_pos)
+            this_eigvals, this_eigvecs = _get_positional_eigvecs(this_L, num_pos=num_pos)
             eigvecs[comp, :] = this_eigvecs
+            eigvals_tile[comp, :] = this_eigvals
     else:
         eigvals, eigvecs = _get_positional_eigvecs(L, num_pos=num_pos)
         eigvals_tile = np.tile(eigvals, (L_norm.shape[0], 1))
@@ -52,26 +54,28 @@ def compute_laplacian_positional_eigvecs(
 
 
 def _get_positional_eigvecs(matrix, num_pos: int):
-    if num_pos < len(matrix) - 1:  # Compute the k-lowest eigenvectors
-        eigvals, eigvecs = splinalg.eigs(matrix, k=num_pos, which="SR", tol=0)
+
+    mat_len = matrix.shape[0]
+    if num_pos < mat_len - 1:  # Compute the k-lowest eigenvectors
+        eigvals, eigvecs = eigs(matrix, k=num_pos, which="SR", tol=0)
 
     else:  # Compute all eigenvectors
 
-        eigvals, eigvecs = splinalg.eig(matrix)
+        eigvals, eigvecs = eig(matrix.todense())
 
         # Pad with non-sense eigenvectors if required
-        if num_pos > len(matrix):
-            temp_EigVal = np.ones(num_pos - len(matrix), dtype=np.float64) + float("inf")
-            temp_EigVec = np.zeros((len(matrix), num_pos - len(matrix)), dtype=np.float64)
+        if num_pos > mat_len:
+            temp_EigVal = np.ones(num_pos - mat_len, dtype=np.float64) + float("inf")
+            temp_EigVec = np.zeros((mat_len, num_pos - mat_len), dtype=np.float64)
             eigvals = np.concatenate([eigvals, temp_EigVal], axis=0)
             eigvecs = np.concatenate([eigvecs, temp_EigVec], axis=1)
 
     # Sort and keep only the first `num_pos` elements
     sort_idx = eigvals.argsort()
     eigvals = eigvals[sort_idx]
+    eigvals = eigvals[:num_pos]
     eigvecs = eigvecs[:, sort_idx]
     eigvecs = eigvecs[:, :num_pos]
-    eigvals = eigvals[:, :num_pos]
 
     # Normalize the eigvecs
     eigvecs = eigvecs / (np.sqrt(np.sum(eigvecs ** 2, axis=0, keepdims=True)) + 1e-8)
