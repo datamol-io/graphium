0dd74df7adc62b7460bc2c2a3d01f3962c25294e
Ubuntu <ubuntu@ip-172-31-29-17.us-east-2.compute.internal>
2021-04-22 00:26:43 +0000

    Added weights in the predictor and the datamodule. Still need to create weighted loss classes

--- goli/trainer/loss_fn.py
+++ goli/trainer/loss_fn.py
@@ -1,11 +1,9 @@
 import torch
 from torch import Tensor
 
+
 class BCELossBalanced(torch.nn.BCELoss):
-    
-    def forward(self, input: Tensor, target:Tensor) -> Tensor:
+    def forward(self, input: Tensor, target: Tensor) -> Tensor:
         assert self.weight is None
 
         dims = list(range(input.ndim - 1))
-
-
