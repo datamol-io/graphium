e0f48ccc5c36986f487b7640a12fcb61d53f5a45
Dominique <dominique@invivoai.com>
2021-04-29 19:32:21 -0400

    Deleted unused file

--- goli/trainer/loss_fn.py
+++
@@ -1,9 +0,0 @@
-import torch
-from torch import Tensor
-
-
-class BCELossBalanced(torch.nn.BCELoss):
-    def forward(self, input: Tensor, target: Tensor) -> Tensor:
-        assert self.weight is None
-
-        dims = list(range(input.ndim - 1))
