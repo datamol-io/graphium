47f72f29b7d694c4ec017f73dec2cab2480594bc
Hadrien Mary <hadrien.mary@gmail.com>
2021-04-30 14:13:51 -0400

    wip cache invalidation

--- goli/features/__init__.py
+++ goli/features/__init__.py
@@ -3,3 +3,4 @@ from .featurizer import get_mol_atomic_features_float
 from .featurizer import get_mol_edge_features
 from .featurizer import mol_to_adj_and_features
 from .featurizer import mol_to_dglgraph
+from .featurizer import mol_to_dglgraph_signature
