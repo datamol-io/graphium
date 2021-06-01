d94c450e9f04104576bf50406bffdee5924068d2
Dominique <dominique@valencediscovery.com>
2021-06-01 01:23:27 +0000

    Ignoring the Nans in the metrics

--- goli/trainer/metrics.py
+++ goli/trainer/metrics.py
@@ -200,7 +200,11 @@ class MetricWrapper:
     """
 
     def __init__(
-        self, metric: Union[str, Callable], threshold_kwargs: Optional[Dict[str, Any]] = None, **kwargs
+        self,
+        metric: Union[str, Callable],
+        threshold_kwargs: Optional[Dict[str, Any]] = None,
+        target_nan_mask: Optional[Union[str, int]] = None,
+        **kwargs,
     ):
         r"""
         Parameters
@@ -212,6 +216,19 @@ class MetricWrapper:
                 Otherwise, we use the class `Thresholder` is initialized with the
                 provided argument, and called before the `compute`
 
+            target_nan_mask:
+
+                - None: Do not change behaviour if there are NaNs
+
+                - int, float: Value used to replace NaNs. For example, if `target_nan_mask==0`, then
+                  all NaNs will be replaced by zeros
+
+                - 'ignore-flatten': The Tensor will be reduced to a vector without the NaN values.
+
+                - 'ignore-mean-label': NaNs will be ignored when computing the loss. Note that each column
+                  has a different number of NaNs, so the metric will be computed separately
+                  on each column, and the metric result will be averaged over all columns.
+
             kwargs:
                 Other arguments to call with the metric
         """
@@ -222,15 +239,54 @@ class MetricWrapper:
         if threshold_kwargs is not None:
             self.thresholder = Thresholder(**threshold_kwargs)
 
+        self.target_nan_mask = target_nan_mask
+
         self.kwargs = kwargs
 
     def compute(self, preds: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
         r"""
-        Compute the metric, and apply the thresholder if provided
+        Compute the metric, apply the thresholder if provided, and manage the NaNs
         """
+
+        target_nans = torch.isnan(target)
+
+        # Threshold the prediction
         if self.thresholder is not None:
             preds, target = self.thresholder(preds, target)
-        metric_val = self.metric(preds, target, **self.kwargs)
+
+        # Manage the NaNs
+        if self.target_nan_mask is None:
+            pass
+        elif isinstance(self.target_nan_mask, (int, float)):
+            target = target.clone()
+            target[torch.isnan(target)] = self.target_nan_mask
+        elif self.target_nan_mask == "ignore-flatten":
+            target = target[~target_nans]
+            preds = preds[~target_nans]
+        elif self.target_nan_mask == "ignore-mean-label":
+            target_list = [target[:, ii][~target_nans[:, ii]] for ii in range(target.shape[-1])]
+            preds_list = [preds[:, ii][~target_nans[:, ii]] for ii in range(preds.shape[-1])]
+            target = target_list
+            preds = preds_list
+        else:
+            raise ValueError(f"Invalid option `{self.target_nan_mask}`")
+
+        if self.target_nan_mask == "ignore-mean-label":
+
+            # Compute the metric for each column, and output nan if there's an error on a given column
+            metric_val = []
+            for ii in range(len(target)):
+                try:
+                    this_metric = self.metric(preds[ii], target[ii], **self.kwargs)
+                except:
+                    this_metric = float("nan") + torch.zeros_like(preds[ii])[..., 0]
+                metric_val.append(this_metric)
+            metric_val = torch.stack(metric_val, dim=-1)
+
+            # Average the metric
+            metric_val = torch.nansum(torch.as_tensor(metric_val)) / torch.sum(~torch.isnan(metric_val))
+        else:
+            metric_val = self.metric(preds, target, **self.kwargs)
         return metric_val
 
     def __call__(self, preds: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
