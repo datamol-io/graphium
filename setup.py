a467f2f4a01b43f7cabf3b01f7ca5862775cadb1
Hadrien Mary <hadrien.mary@gmail.com>
2021-07-16 17:36:51 -0400

    bumped version to 0.2.1

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ install_requires = [
 
 setup(
     name="goli",
-    version="0.2.0",
+    version="0.2.1",
     author="Valence Discovery",
     author_email="dominique@valencediscovery.com",
     url="https://github.com/valence-discovery/goli",
