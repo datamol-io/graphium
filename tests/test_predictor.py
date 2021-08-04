b06be76c712f803bdf81c75fc220c76529d7bb63
Dominique <dominique@invivoai.com>
2021-08-04 15:11:02 -0400

    Added the MetricWrapper to the loss computation to allow better NaN handling

---
+++ tests/test_predictor.py
@@ -0,0 +1,24 @@
+"""
+Unit tests for the file goli/trainer/predictor.py
+"""
+
+import torch
+from torch.nn import BCELoss, MSELoss
+import unittest as ut
+
+from goli.trainer.predictor import PredictorModule
+
+
+class test_Predictor(ut.TestCase):
+    def test_parse_loss_fun(self):
+        losses = ["bce", "mse", "mae", "l1", BCELoss(), MSELoss()]
+        preds = torch.rand(10, 5)
+        target = (torch.rand(10, 5) > 0.5).to(preds.dtype)
+        for this_loss in losses:
+            loss_fun = PredictorModule.parse_loss_fun(this_loss)
+            print(this_loss)
+            loss = loss_fun(preds, target)
+
+
+if __name__ == "__main__":
+    ut.main()
