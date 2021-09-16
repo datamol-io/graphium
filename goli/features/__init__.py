f8585686bc5026287fe08644168889209503413b
Dominique <dominique@valencediscovery.com>
2021-09-16 02:13:43 +0000

    Generating dict instead of dgl graphs to reduce memory

--- goli/features/__init__.py
+++ goli/features/__init__.py
@@ -4,4 +4,5 @@ from .featurizer import get_mol_edge_features
 from .featurizer import mol_to_adj_and_features
 from .featurizer import mol_to_dglgraph_dict
 from .featurizer import mol_to_dglgraph
+from .featurizer import dgl_dict_to_graph
 from .featurizer import mol_to_dglgraph_signature
