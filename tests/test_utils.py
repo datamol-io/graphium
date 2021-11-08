fb9c61803e2f90e931d6fdd4d19dd6545491c959
Dominique <dominique@invivoai.com>
2021-11-08 14:32:41 -0500

    Added unit test for nan_median

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@
 Unit tests for the metrics and wrappers of goli/utils/...
 """
 
-from goli.utils.tensor import nan_mean, nan_std, nan_var
+from goli.utils.tensor import nan_mean, nan_std, nan_var, nan_median
 import torch
 import numpy as np
 import unittest as ut
@@ -77,6 +77,27 @@ class test_nan_statistics(ut.TestCase):
                     numpy_var = np.nanvar(tensor.numpy(), **numpy_kwargs)
                     np.testing.assert_almost_equal(torch_var.numpy(), numpy_var, decimal=6, err_msg=err_msg)
 
+
+    def test_nan_median(self):
+
+        for keepdim in [False, True]:
+            for dim in self.dims:
+                err_msg = f"Error for :\n dim = {dim}\n keepdim = {keepdim}"
+
+                tensor = self.tensor.clone()
+
+                # Prepare the arguments for numpy vs torch
+                if dim is not None:
+                    torch_kwargs = {"dim": dim, "keepdim": keepdim}
+                    numpy_kwargs = {"axis": dim, "keepdims": keepdim}
+                else:
+                    torch_kwargs = {}
+                    numpy_kwargs = {}
+
+                # Compare the nan-mean
+                torch_med = nan_median(tensor, **torch_kwargs)
+                numpy_med = np.nanmedian(tensor.numpy(), **numpy_kwargs)
+                np.testing.assert_almost_equal(torch_med.numpy(), numpy_med, decimal=6, err_msg=err_msg)
 
 if __name__ == "__main__":
     ut.main()
