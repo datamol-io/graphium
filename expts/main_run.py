bfb75c205a763084ce517a54e0b22ffc9f939679
DomInvivo <dominique@invivoai.com>
2021-11-08 16:42:26 +0000

    Fixed memory leak due to keeping predictions from all training steps

--- expts/main_run.py
+++ expts/main_run.py
@@ -11,14 +11,19 @@ import goli
 from goli.config._loader import load_datamodule, load_metrics, load_architecture, load_predictor, load_trainer
 
 
+
 # Set up the working directory
 MAIN_DIR = dirname(dirname(abspath(goli.__file__)))
 # CONFIG_FILE = "expts/config_molPCBA.yaml"
 # CONFIG_FILE = "expts/config_molHIV.yaml"
 # CONFIG_FILE = "expts/config_molPCQM4M.yaml"
 # CONFIG_FILE = "expts/config_micro_ZINC.yaml"
-CONFIG_FILE = "expts/config_micro-PCBA.yaml"
+# CONFIG_FILE = "expts/config_micro-PCBA.yaml"
 # CONFIG_FILE = "expts/config_ZINC_bench_gnn.yaml"
+# CONFIG_FILE = "expts_untracked/config_htsfp_pcba.yaml"
+# CONFIG_FILE = "expts_untracked/config_mega_pubchem.yaml"
+CONFIG_FILE = "expts_untracked/config_mega_pubchem_L1000.yaml"
+
 os.chdir(MAIN_DIR)
 
 
