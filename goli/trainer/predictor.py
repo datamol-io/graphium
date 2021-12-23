759cf69dbaad156f2d2e12ec48571c7b4cc0fb87
Gabriela Moisescu-Pareja <gabriela@valencediscovery.com>
2021-12-23 17:01:17 +0000

    feat: Implement FLAG (adversarial data augmentation) for training GNNs
    Implement FLAG (Free Large-Scale Adversarial Augmentation on Graphs) to boost performance of various GNNs across various molecular tasks. FLAG performs adversarial data augmentation by iteratively augmenting node features with gradient-based adversarial perturbations during training.
    
    * Add `n_flag_steps` attribute to `PredictorModule` to specify how many times to loop FLAG (this attribute may be modified from any config file in `goli/expts` by changing `n_flag_steps` in `predictor`)
    - Default value 0 trains GNNs without using FLAG
    - Value > 0 will loop FLAG that many times
    
    Fixes: Implement FLAG #77

--- goli/trainer/predictor.py
+++ goli/trainer/predictor.py
@@ -131,6 +131,7 @@ class PredictorModule(pl.LightningModule):
         metrics: Dict[str, Callable] = None,
         metrics_on_progress_bar: List[str] = [],
         metrics_on_training_set: Optional[List[str]] = None,
+        n_flag_steps: int = 0,
     ):
         r"""
         A class that allows to use regression or classification models easily
@@ -224,6 +225,7 @@ class PredictorModule(pl.LightningModule):
         self.lr_reduce_on_plateau_kwargs = lr_reduce_on_plateau_kwargs
         self.optim_kwargs = optim_kwargs
         self.scheduler_kwargs = scheduler_kwargs
+        self.n_flag_steps = n_flag_steps
 
         # Set the default value for the optimizer
         self.optim_kwargs = optim_kwargs if optim_kwargs is not None else {}
@@ -447,10 +449,86 @@ class PredictorModule(pl.LightningModule):
         step_dict[f"{self.loss_fun._get_name()}/{step_name}"] = loss.detach().cpu()
         return loss, step_dict
 
+    def flag_step(
+        self, 
+        batch: Dict[str, Tensor], 
+        batch_idx: int, 
+        step_name: str, 
+        to_cpu: bool, 
+        M: int, 
+        alpha: float
+    ) -> Dict[str, Any]:
+
+        X = self._convert_features_dtype(batch['features'])
+        X_shape = X.ndata['feat'].shape
+
+        pert = torch.FloatTensor(X_shape).uniform_(-alpha, alpha).to(device=X.device)
+        pert.requires_grad = True
+
+        # Perturb the features
+        pert_batch = deepcopy(batch)
+        pert_batch['features'] = pert_batch["features"]
+        pert_batch['labels'] = pert_batch['labels']
+        pert_batch['features'].ndata['feat'] = batch['features'].ndata['feat'] + pert
+        
+        preds = self.forward(pert_batch)
+        targets = batch.pop("labels").to(dtype=preds.dtype)
+        weights = batch.pop("weights", None)
+        loss = self.compute_loss(
+           preds=preds,
+           targets=targets,
+           weights=weights,
+           target_nan_mask=self.target_nan_mask,
+           loss_fun=self.loss_fun,
+       )/M
+
+        # Iteratively augment data by applying perturbations
+        # Accumulate the gradients to be applied to the weights of the network later on
+        for _ in range(M-1):
+            loss.backward()
+            pert_data = pert.detach() + alpha*torch.sign(pert.grad.detach())
+            pert.data = pert_data.data # This is not actually good anymore (look at SE posts)
+            pert.grad[:] = 0
+            pert_batch['features'].ndata['feat'] = batch['features'].ndata['feat'] + pert
+            preds = self.forward(pert_batch)
+            loss = self.compute_loss(
+           		preds=preds,
+           		targets=targets,
+           		weights=weights,
+           		target_nan_mask=self.target_nan_mask,
+           		loss_fun=self.loss_fun,
+       	    )/M
+        
+        device = "cpu" if to_cpu else None
+        preds = preds.detach().to(device=device)
+        targets = targets.detach().to(device=device)
+        if weights is not None:
+            weights = weights.detach().to(device=device)
+
+        step_dict = {"preds": preds, "targets": targets, "weights": weights}
+        step_dict[f"{self.loss_fun._get_name()}/{step_name}"] = loss.detach().cpu()
+        return loss, step_dict
+
     def training_step(self, batch: Dict[str, Tensor], batch_idx: int) -> Dict[str, Any]:
-        loss, step_dict = self._general_step(
-            batch=batch, batch_idx=batch_idx, step_name="train", to_cpu=False
-        )
+        loss = None
+        step_dict = None
+
+        # Train using FLAG
+        if (self.n_flag_steps > 0):
+            loss, step_dict = self.flag_step(
+                batch=batch, 
+                batch_idx=batch_idx, 
+                step_name="train", 
+                to_cpu=False, 
+                M=self.n_flag_steps, 
+                alpha=0.001
+            )
+        # Train normally, without using FLAG
+        elif (self.n_flag_steps == 0):
+            loss, step_dict = self._general_step(
+                batch=batch, batch_idx=batch_idx, step_name="train", to_cpu=True
+            )
+
         metrics_logs = self.get_metrics_logs(
             preds=step_dict["preds"],
             targets=step_dict["targets"],
