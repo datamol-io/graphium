7802c5b0312e7b3697ea0b3a3a3aaa92a4d48fc4
DomInvivo <dominique@invivoai.com>
2022-03-31 21:10:07 -0400

    Improved speed ~3 fold for mol->graph

---
+++ profiling/profile_mol_to_graph.py
@@ -0,0 +1,43 @@
+from tqdm import tqdm
+import datamol as dm
+
+from goli.data.utils import load_micro_zinc
+from goli.features.featurizer import mol_to_dglgraph, mol_to_adj_and_features
+
+def main():
+    df = load_micro_zinc()
+    smiles = df["SMILES"].values
+    print("Num smiles: ", len(smiles))
+
+    pos_enc = {
+        "pos_type": "laplacian_eigvec",
+        "num_pos": 3,
+        "normalization": "none",
+        "disconnected_comp": True,
+    }
+    atom_property_list_float1 = ["mass", "in-ring", "hybridization", "chirality", "aromatic",
+                                 "degree", "formal-charge", "single-bond", "double-bond", "radical-electron",
+                                 ]
+    atom_property_list_float2 = ["electronegativity",
+                                 "vdw-radius", "covalent-radius", "metal"]
+
+    featurizer = {
+        "atom_property_list_onehot": ["atomic-number", "valence"],
+        "atom_property_list_float": atom_property_list_float1 + atom_property_list_float2,
+        "edge_property_list": ["bond-type-onehot", "bond-type-float", "stereo", "in-ring", "conjugated", "estimated-bond-length"],
+        "add_self_loop": False,
+        "explicit_H": False,
+        "use_bonds_weights": False,
+        "pos_encoding_as_features": pos_enc,
+        "pos_encoding_as_directions": pos_enc,
+        }
+
+    for s in tqdm(smiles):
+        mol = dm.to_mol(s)
+        graph = mol_to_adj_and_features(mol, **featurizer)
+
+    print(graph)
+
+
+if __name__ == "__main__":
+    main()
