594cd884219fe8db45233fd25629c0b53f0ad873
DomInvivo <dominique@invivoai.com>
2022-06-02 11:58:53 -0400

    Improved profiling of the forward loop

--- profiling/profile_forward.py
+++ profiling/profile_forward.py
@@ -1,28 +1,23 @@
-from tqdm import tqdm
-import os
 from time import time
 import torch
-import numpy as np
 import yaml
 import fsspec
 import dgl
 
-from goli.data.utils import load_micro_zinc
-from goli.trainer.predictor import PredictorModule
 
-from goli.config._loader import load_datamodule, load_metrics, load_trainer
-from pytorch_lightning import Trainer
+from goli.config._loader import load_datamodule, load_metrics, load_predictor, load_architecture
 
 
 def main():
-    MODEL_PATH = "gs://goli-private/pretrained-models/micro_model/model.ckpt"
-    CONFIG_PATH = "gs://goli-private/pretrained-models/micro_model/configs.yaml"
+    CONFIG_PATH = "configs_profiling.yaml"
     DATA_PATH = "https://storage.googleapis.com/goli-public/datasets/goli-zinc-bench-gnn/smiles_score.csv.gz"
     BATCH_SIZE = 1000
+    NUM_FORWARD_LOOPS = 30
 
     with fsspec.open(CONFIG_PATH, "r") as f:
         cfg = yaml.safe_load(f)
-    cfg["datamodule"]["args"]["cache_data_path"] = "goli/data/cache/profiling_forward_data.cache"
+    cfg["datamodule"]["args"]
+    cfg["datamodule"]["args"]["cache_data_path"] = "goli/data/cache/profiling/forward_data.cache"
     cfg["datamodule"]["args"]["df_path"] = DATA_PATH
     cfg["datamodule"]["args"]["sample_size"] = BATCH_SIZE
     cfg["datamodule"]["args"]["prepare_dict_or_graph"] = "dglgraph"
@@ -35,16 +30,23 @@ def main():
     dglgraphs = dgl.batch(dglgraphs)
     dglgraphs.ndata["feat"] = dglgraphs.ndata["feat"].to(dtype=torch.float32)
     dglgraphs.edata["feat"] = dglgraphs.edata["feat"].to(dtype=torch.float32)
-    with fsspec.open(MODEL_PATH) as f:
-        model = torch.load(f, map_location="cpu")
-    predictor = PredictorModule.load_from_checkpoint(MODEL_PATH)
+
+    # Initialize the network
+    model_class, model_kwargs = load_architecture(
+        cfg,
+        in_dim_nodes=datamodule.num_node_feats_with_positional_encoding,
+        in_dim_edges=datamodule.num_edge_feats,
+    )
+    metrics = load_metrics(cfg)
+
+    predictor = load_predictor(cfg, model_class, model_kwargs, metrics)
     model = predictor.model
 
     start = time()
-    for ii in range(10):
+    for ii in range(NUM_FORWARD_LOOPS):
         print(ii)
-        model.forward(dglgraphs)
-    print(time() - start)
+        model._forward(dglgraphs, flip_pos_enc="random")
+    print(f"Time to {NUM_FORWARD_LOOPS} forward loops: {time() - start} s")
 
     print("Done :)")
 
