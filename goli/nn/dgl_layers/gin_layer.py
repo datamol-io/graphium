8f03cd210158481f4cdbb0ef8d5c22179389b2c3
DomInvivo <dominique@invivoai.com>
2022-06-21 21:12:43 -0400

    Moved base_graph_layer

--- goli/nn/dgl_layers/gin_layer.py
+++ goli/nn/dgl_layers/gin_layer.py
@@ -3,7 +3,7 @@ import dgl.function as fn
 from dgl import DGLGraph
 from typing import Callable, Union
 
-from goli.nn.dgl_layers.base_dgl_layer import BaseDGLLayer
+from goli.nn.base_graph_layer import BaseGraphLayer
 from goli.nn.base_layers import MLP
 from goli.utils.decorators import classproperty
 
@@ -14,7 +14,7 @@ from goli.utils.decorators import classproperty
 """
 
 
-class GINLayer(BaseDGLLayer):
+class GINLayer(BaseGraphLayer):
     def __init__(
         self,
         in_dim: int,
