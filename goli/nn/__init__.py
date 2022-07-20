f2417c1fd0a42e014689d2bb9a9f6fef232f51f5
DomInvivo <dominique@invivoai.com>
2022-07-19 22:14:17 -0400

    Merging changes from Master

--- goli/nn/__init__.py
+++ goli/nn/__init__.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 from .architectures import FullDGLNetwork
 from .architectures import FullDGLSiameseNetwork
 from .architectures import FeedForwardNN
 from .architectures import FullDGLMultiTaskNetwork
->>>>>>> origin/master
