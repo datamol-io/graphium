c2a5ef69faa37215214c733736fa0efbc97a47ca
shenyangHuang <shenyang.huang@mail.mcgill.ca>
2022-07-21 18:44:41 +0000

    starting a new branch for ipu (based on the ipu branch)

--- venv.sh
+++ venv.sh
@@ -1,5 +1,6 @@
-pip3 install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
-pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.12.0+cpu.html
+pip install torch==1.10.0 torchaudio==0.10.0
+pip install torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.10.0+cpu.html
 pip install dgl dglgo -f https://data.dgl.ai/wheels/repo.html
+pip install /opt/gc/sdk-2.5.1/poptorch-2.5.0+62285_0f4af0bf32_ubuntu_20_04-cp38-cp38-linux_x86_64.whl
 pip install -r requirements.txt 
 pip install -e .
