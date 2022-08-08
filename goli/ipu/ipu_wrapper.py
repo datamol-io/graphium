44d06de3b373f5695f6348fb56b6087a227fa45d
Gabriela Moisescu-Pareja <gabriela@valencediscovery.com>
2022-08-08 14:12:49 -0400

    Move functionality of the multitask predictor to  instead of  (which should be deleted), clean up code and modify test

--- goli/ipu/ipu_wrapper.py
+++ goli/ipu/ipu_wrapper.py
@@ -6,7 +6,7 @@ from inspect import _ParameterKind
 from pytorch_lightning.plugins import IPUPlugin
 from pytorch_lightning.trainer.states import RunningStage
 
-from goli.trainer.refactor_predictor_mtl import PredictorModule
+from goli.trainer.predictor import PredictorModule
 from goli.ipu.ipu_utils import import_poptorch
 
 
@@ -17,7 +17,7 @@ always reduce the last loss since it is the fake graph
 def remove_pad_loss(preds: Dict[str, Tensor], targets: Dict[str, Tensor]):
     for task in targets.keys():
         if (targets[task].shape == preds[task].shape):
-            continue 
+            continue
         else:
             preds[task] = preds[task][:-1]
     return preds
