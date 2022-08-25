ac1944ba6a8e60bee9cc9060f4d7058b90359366
DomInvivo <dominique@valencediscovery.com>
2022-08-25 15:16:19 +0000

    commit before pull

--- goli/data/datamodule.py
+++ goli/data/datamodule.py
@@ -420,8 +420,6 @@ class BaseDataModule(pl.LightningDataModule):
 
         return loader
 
-<<<<<<< HEAD
-=======
 class GraphFromSmilesDataModule(BaseDataModule): #TODO: DELETE
     """
     NOTE(hadim): let's make only one class for the moment and refactor with a parent class
@@ -1151,7 +1149,6 @@ class GraphFromSmilesDataModule(BaseDataModule): #TODO: DELETE
         """Controls how the class is printed"""
         return omegaconf.OmegaConf.to_yaml(self.to_dict())
 
->>>>>>> origin/ipu
 class DatasetProcessingParams():
    def __init__(
        self,
