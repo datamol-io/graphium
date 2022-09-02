c67f4dc7ac5182d970a5a8638edcecf5c0a6e840
DomInvivo <dominique@valencediscovery.com>
2022-09-02 20:26:13 +0000

    Added custom WandbLogger

--- goli/trainer/loggers.py
+++ goli/trainer/loggers.py
@@ -2,7 +2,7 @@ import os
 import yaml
 from copy import deepcopy
 
-from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.loggers import TensorBoardLogger, WandbLogger
 from pytorch_lightning.utilities.rank_zero import rank_zero_only
 
 
@@ -18,4 +18,19 @@ class TensorBoardLoggerGoli(TensorBoardLogger):
             yaml.dump(self.full_configs, file)
 
         return super().log_hyperparams(*args, **kwargs)
+
+
+class WandbLoggerGoli(WandbLogger):
+    def __init__(self, full_configs=None, *args, **kwargs):
+        self.full_configs = deepcopy(full_configs)
+        super().__init__(*args, **kwargs)
+
+    @rank_zero_only
+    def log_hyperparams(self, *args, **kwargs) -> None:
+
+        with open(os.path.join(self.log_dir, "configs.yaml"), 'w') as file:
+            yaml.dump(self.full_configs, file)
+
+        return super().log_hyperparams(*args, **kwargs)
+
 
