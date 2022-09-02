9b2a95a1a66f667d3cc5cc804b2e2e781dd83226
DomInvivo <dominique@valencediscovery.com>
2022-09-02 20:47:07 +0000

    Cleaned-up the predictor

--- goli/trainer/predictor.py
+++ goli/trainer/predictor.py
@@ -83,7 +83,6 @@ class PredictorModule(pl.LightningModule):
         self.model = self._model_options.model_class(**self._model_options.model_kwargs)
         self.tasks = list(loss_fun.keys())
 
-###########################################################################################################################################
         # Task-specific evalutation attributes
         self.loss_fun = {}
         self.metrics = {}
@@ -96,7 +95,6 @@ class PredictorModule(pl.LightningModule):
             self.metrics_on_training_set[task] = (
                 list(self.metrics[task].keys()) if self._eval_options_dict[task].metrics_on_training_set is None else self._eval_options_dict[task].metrics_on_training_set
             )
-###########################################################################################################################################
         self.n_params = sum(p.numel() for p in self.parameters() if p.requires_grad)
 
         # Set the parameters and default values for the FLAG adversarial augmentation, and check values
@@ -112,7 +110,6 @@ class PredictorModule(pl.LightningModule):
         # Set the pytorch scheduler arguments
         self.torch_scheduler_kwargs = self._optim_options.torch_scheduler_kwargs
 
-###########################################################################################################################################
         # Initialize the epoch summary
         monitor = "micro_zinc/MSELoss/val" #self.scheduler_kwargs["monitor"].split("/")[0] TODO: Fix the scheduler with the Summary class
         mode = "min" #self.scheduler_kwargs["mode"]
@@ -125,7 +122,6 @@ class PredictorModule(pl.LightningModule):
             monitor=monitor,
             mode=mode,
         )
-###########################################################################################################################################
 
         # This helps avoid a bug when saving hparams to yaml with different dict or str formats
         self._set_hparams(recursive_config_reformating(self.hparams))
@@ -389,7 +385,6 @@ class PredictorModule(pl.LightningModule):
                 batch=batch, step_name="train", to_cpu=True
             )
 
-#################################################################################################################
         self.task_epoch_summary.update_predictor_state(
             step_name="train",
             targets=step_dict["targets"],
@@ -402,12 +397,11 @@ class PredictorModule(pl.LightningModule):
         step_dict.update(metrics_logs)          # Dict[task, metric_logs]. Concatenate them?
 
         concatenated_metrics_logs = self.task_epoch_summary.concatenate_metrics_logs(metrics_logs)
-        # TODO add wandb metric tracking here (check in concatenated_metrics_logs)
-        self.logger.log_metrics(concatenated_metrics_logs, step=self.global_step)            # This is a pytorch lightning function call
-#################################################################################################################
 
+        # Wandb metric tracking here
         step_dict["grad_norm"] = self.get_gradient_norm()
-        # print("grad_norm", step_dict["grad_norm"]) # TODO: Remove grad_norm
+        concatenated_metrics_logs["train/grad_norm"] = step_dict["grad_norm"]
+        self.logger.log_metrics(concatenated_metrics_logs, step=self.global_step)
 
         # # Predictions and targets are no longer needed after the step.
         # # Keeping them will increase memory usage significantly for large datasets.
@@ -453,7 +447,7 @@ class PredictorModule(pl.LightningModule):
             target_nan_mask=self.target_nan_mask,
             loss_fun=self.loss_fun,
         )
-################################################################################################################
+
         self.task_epoch_summary.update_predictor_state(
             step_name=step_name,
             predictions=preds,
@@ -464,7 +458,7 @@ class PredictorModule(pl.LightningModule):
         )
         metrics_logs = self.task_epoch_summary.get_metrics_logs()
         self.task_epoch_summary.set_results(task_metrics=metrics_logs)
-################################################################################################################
+
         return metrics_logs             # Consider returning concatenated dict for tensorboard
 
 
@@ -489,13 +483,6 @@ class PredictorModule(pl.LightningModule):
         # Save yaml file with the per-task metrics summaries
         full_dict = {}
         full_dict.update(self.task_epoch_summary.get_dict_summary())
-        # tb_path = self.logger.log_dir
-
-        # # Write the YAML file with the per-task metrics
-        # if self.current_epoch >= 0:
-        #     mkdir(tb_path)
-        #     with open(os.path.join(tb_path, "metrics.yaml"), "w") as file:
-        #         yaml.dump(full_dict, file)
 
     def test_epoch_end(self, outputs: Dict[str, Any]):
 
@@ -507,10 +494,6 @@ class PredictorModule(pl.LightningModule):
         # Save yaml file with the per-task metrics summaries
         full_dict = {}
         full_dict.update(self.task_epoch_summary.get_dict_summary())
-        # tb_path = self.logger.log_dir
-        # os.makedirs(tb_path, exist_ok=True)
-        # with open(f"{tb_path}/metrics.yaml", "w") as file:
-        #     yaml.dump(full_dict, file)
 
     def on_train_start(self):
         hparams_log = deepcopy(self.hparams)
