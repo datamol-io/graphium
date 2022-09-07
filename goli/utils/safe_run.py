778d1c6c4b0dfb7ed1e967e670cb4048f0923c1e
DomInvivo <dominique@valencediscovery.com>
2022-09-07 19:21:32 +0000

    Added docs to safe_run

--- goli/utils/safe_run.py
+++ goli/utils/safe_run.py
@@ -3,27 +3,46 @@ from loguru import logger
 import traceback as tb
 
 class SafeRun():
-    def __init__(self, name: str, raise_error: bool = True, verbose: bool = True) -> None:
+    def __init__(self, name: str, raise_error: bool = True, verbose: int = 2) -> None:
+        """
+        Run some code with error handling and some printing, using the with statment.
+
+        Example:
+            In the example below, the `2+None`, an error will be caught and printed.
+            ```
+            with SafeRun("Addition that fails", raise_error=False):
+                2 + None
+            ```
+
+        Parameters:
+            name: Name of the code, used for printing
+            raise_error: Whether to raise an error, or to catch it and print instead
+            verbose: The level of verbosity
+                0: Do not print anything
+                1: Print only the traced stack when an error is caught and `raise_error` is False
+                2: Print headers and footers at the start and exit of the with statement.
+        """
         self.name = name
         self.raise_error = raise_error
         self.verbose = verbose
 
     def __enter__(self):
-        if self.verbose:
+        if self.verbose >= 2:
             logger.info(f"\n------------ {self.name} STARTED ------------")
 
 
     def __exit__(self, type, value, traceback):
         if traceback is not None:
             if self.raise_error:
-                logger.error(f"------------ {self.name} ERROR: ------------")
+                if self.verbose >=1:
+                    logger.error(f"------------ {self.name} ERROR: ------------")
                 return False
             else:
-                if self.verbose:
+                if self.verbose >=1:
                     logger.error(f"------------ {self.name} ERROR: ------------\nERROR skipped. Traceback:\n")
                     logger.trace(print(''.join(tb.format_exception(None, value, traceback))))
                 return True
         else:
-            if self.verbose:
+            if self.verbose >=2:
                 logger.info("\n------------ {self.name} COMPLETED ------------\n\n")
             return True
