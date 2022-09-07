dff472b688b1a5acf895c44aeaac9eda9d599508
DomInvivo <dominique@valencediscovery.com>
2022-09-07 13:54:07 +0000

    Moved files to 'scripts' folder. Removed redundant code from _loader

--- goli/config/_loader.py
+++ goli/config/_loader.py
@@ -159,26 +159,18 @@ def load_architecture(
         gnn_kwargs.setdefault("in_dim_edges", in_dim_edges)
 
     # Set the parameters for the full network
-    if task_heads_kwargs is None:
-        model_kwargs = dict(
-            gnn_kwargs=gnn_kwargs,
-            pre_nn_kwargs=pre_nn_kwargs,
-            pre_nn_edges_kwargs=pre_nn_edges_kwargs,
-            post_nn_kwargs=post_nn_kwargs,
-        )
-    else:
-        task_head_params_list = []
-        for params in omegaconf.OmegaConf.to_object(task_heads_kwargs): # This turns the ListConfig into List[TaskHeadParams]
-            params_dict = dict(params)
-            task_head_params_list.append(params_dict)
+    task_head_params_list = []
+    for params in omegaconf.OmegaConf.to_object(task_heads_kwargs): # This turns the ListConfig into List[TaskHeadParams]
+        params_dict = dict(params)
+        task_head_params_list.append(params_dict)
 
-        model_kwargs = dict(
-            gnn_kwargs=gnn_kwargs,
-            pre_nn_kwargs=pre_nn_kwargs,
-            pre_nn_edges_kwargs=pre_nn_edges_kwargs,
-            post_nn_kwargs=post_nn_kwargs,
-            task_heads_kwargs_list=task_head_params_list,
-        )
+    model_kwargs = dict(
+        gnn_kwargs=gnn_kwargs,
+        pre_nn_kwargs=pre_nn_kwargs,
+        pre_nn_edges_kwargs=pre_nn_edges_kwargs,
+        post_nn_kwargs=post_nn_kwargs,
+        task_heads_kwargs_list=task_head_params_list,
+    )
 
     return model_class, model_kwargs
 
