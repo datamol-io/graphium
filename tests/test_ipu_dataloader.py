f3c5c69ff939da1f62f09146484b372d4cde657c
DomInvivo <dominique@valencediscovery.com>
2022-09-22 19:57:51 +0000

    black linting

--- tests/test_ipu_dataloader.py
+++ tests/test_ipu_dataloader.py
@@ -19,6 +19,7 @@ from goli.ipu.ipu_wrapper import IPUPluginGoli
 from goli.ipu.ipu_dataloader import CombinedBatchingCollator
 from goli.data.collate import goli_collate_fn
 
+
 def random_packing(num_nodes, batch_size):
     ipu_batch_size = int(len(num_nodes) / batch_size)
     indices = np.arange(len(num_nodes))
@@ -26,14 +27,14 @@ def random_packing(num_nodes, batch_size):
     indices = np.reshape(indices, (ipu_batch_size, batch_size)).tolist()
     return indices
 
+
 def global_batch_collator(batch_size, batches):
     packs = []
     for pack_idx in range(0, len(batches), batch_size):
-        packs.append(default_collate(batches[pack_idx:pack_idx+batch_size]))
+        packs.append(default_collate(batches[pack_idx : pack_idx + batch_size]))
     global_batch = default_collate(packs)
     global_batch = (global_batch[0], tuple(global_batch[1]))
     return global_batch
-
 
 
 class test_SmartPacking(ut.TestCase):
@@ -78,10 +79,10 @@ class test_SmartPacking(ut.TestCase):
                     np.arange(len(num_nodes)).tolist(),
                 )
 
-class test_DataLoading(ut.TestCase):
 
+class test_DataLoading(ut.TestCase):
     class TestSimpleLightning(LightningModule):
-    # Create a basic Ligthning for testing the batch sizes
+        # Create a basic Ligthning for testing the batch sizes
         def __init__(self, batch_size, node_feat_size, edge_feat_size, num_batch) -> None:
             super().__init__()
             self.batch_size = batch_size
@@ -112,21 +113,26 @@ class test_DataLoading(ut.TestCase):
             # Test the shape of the labels
             this_shape = list(batch[0].shape)
             true_shape = [1, self.batch_size]
-            assert this_shape == true_shape, f"Shape of the labels is `{this_shape}` but should be {true_shape}. {msg}"
+            assert (
+                this_shape == true_shape
+            ), f"Shape of the labels is `{this_shape}` but should be {true_shape}. {msg}"
 
             # Test the shape of the first feature
             this_shape = list(batch[1][0].shape)
             true_shape = [1, self.batch_size, self.node_feat_size]
-            assert this_shape == true_shape, f"Shape of the feature 0 is `{this_shape}` but should be {true_shape}. {msg}"
+            assert (
+                this_shape == true_shape
+            ), f"Shape of the feature 0 is `{this_shape}` but should be {true_shape}. {msg}"
 
             # Test the shape of the second feature
             this_shape = list(batch[1][1].shape)
             true_shape = [1, self.batch_size, self.edge_feat_size]
-            assert this_shape == true_shape, f"Shape of the feature 0 is `{this_shape}` but should be {true_shape}. {msg}"
+            assert (
+                this_shape == true_shape
+            ), f"Shape of the feature 0 is `{this_shape}` but should be {true_shape}. {msg}"
 
         def configure_optimizers(self):
             return torch.optim.Adam(self.parameters(), lr=1e-3)
-
 
     class TestPredictor(PredictorModuleIPU):
         # Create a basic Ligthning for testing the batch sizes
@@ -138,13 +144,16 @@ class test_DataLoading(ut.TestCase):
             self.edge_batch_size = edge_batch_size
 
             import poptorch
+
             self.pop = poptorch
 
         def validation_step(self, *args):
             dict_input = self._build_dict_input(*args)
             self.assert_shapes(args, dict_input, "val")
             preds = self.forward(dict_input)["preds"]
-            loss = self.compute_loss(preds, dict_input["labels"], weights=None, loss_fun=self.loss_fun, target_nan_mask=None)
+            loss = self.compute_loss(
+                preds, dict_input["labels"], weights=None, loss_fun=self.loss_fun, target_nan_mask=None
+            )
             loss = loss[0]
             return loss
 
@@ -152,7 +161,9 @@ class test_DataLoading(ut.TestCase):
             dict_input = self._build_dict_input(*args)
             self.assert_shapes(args, dict_input, "train")
             preds = self.forward(dict_input)["preds"]
-            loss = self.compute_loss(preds, dict_input["labels"], weights=None, loss_fun=self.loss_fun, target_nan_mask=None)
+            loss = self.compute_loss(
+                preds, dict_input["labels"], weights=None, loss_fun=self.loss_fun, target_nan_mask=None
+            )
             loss = self.poptorch.identity_loss(loss[0], reduction="mean")
             return loss
 
@@ -166,17 +177,23 @@ class test_DataLoading(ut.TestCase):
             # Test the shape of the labels
             this_shape = list(dict_input["labels"]["homo"].shape)
             true_shape = [self.batch_size, 2]
-            assert this_shape == true_shape, f"Shape of the labels is `{this_shape}` but should be {true_shape}. {msg}"
+            assert (
+                this_shape == true_shape
+            ), f"Shape of the labels is `{this_shape}` but should be {true_shape}. {msg}"
 
             # Test the shape of the node feature
             this_shape = list(dict_input["features"]["feat"].shape)
             true_shape = [self.node_batch_size, self.in_dims["feat"]]
-            assert this_shape == true_shape, f"Shape of the feature 0 is `{this_shape}` but should be {true_shape}. {msg}"
+            assert (
+                this_shape == true_shape
+            ), f"Shape of the feature 0 is `{this_shape}` but should be {true_shape}. {msg}"
 
             # Test the shape of the node feature
             this_shape = list(dict_input["features"]["edge_feat"].shape)
             true_shape = [self.edge_batch_size, self.in_dims["edge_feat"]]
-            assert this_shape == true_shape, f"Shape of the feature 0 is `{this_shape}` but should be {true_shape}. {msg}"
+            assert (
+                this_shape == true_shape
+            ), f"Shape of the feature 0 is `{this_shape}` but should be {true_shape}. {msg}"
 
         def get_progress_bar_dict(self):
             return {}
@@ -196,10 +213,9 @@ class test_DataLoading(ut.TestCase):
         def configure_optimizers(self):
             return torch.optim.Adam(self.parameters(), lr=1e-3)
 
-
     class TestDataset(torch.utils.data.Dataset):
         # Create a simple dataset for testing the Lightning integration
-        def __init__( self, labels, node_features, edge_features):
+        def __init__(self, labels, node_features, edge_features):
             self.labels = labels
             self.node_features = node_features
             self.edge_features = edge_features
@@ -210,7 +226,6 @@ class test_DataLoading(ut.TestCase):
         def __getitem__(self, idx):
             # [label, [feat1, feat2]]
             return [self.labels[idx], [self.node_features[idx], self.edge_features[idx]]]
-
 
     def test_poptorch_simple_deviceiterations_gradient_accumulation(self):
         # Run this test only if poptorch is available
@@ -232,17 +247,17 @@ class test_DataLoading(ut.TestCase):
         opts = poptorch.Options()
         opts.deviceIterations(device_iterations)
         opts.Jit.traceModel(True)
-        training_opts=deepcopy(opts)
+        training_opts = deepcopy(opts)
         training_opts.Training.gradientAccumulation(gradient_accumulation)
-        inference_opts=deepcopy(opts)
+        inference_opts = deepcopy(opts)
 
         # Initialize the dataset
         num_batch = device_iterations * gradient_accumulation * num_replicate
         data_size = num_batch * batch_size
         dataset = self.TestDataset(
-            labels = np.random.rand(data_size).astype(np.float32),
-            node_features = [np.random.rand(node_feat_size).astype(np.float32) for ii in range(data_size)],
-            edge_features = [np.random.rand(edge_feat_size).astype(np.float32) for ii in range(data_size)],
+            labels=np.random.rand(data_size).astype(np.float32),
+            node_features=[np.random.rand(node_feat_size).astype(np.float32) for ii in range(data_size)],
+            edge_features=[np.random.rand(edge_feat_size).astype(np.float32) for ii in range(data_size)],
         )
 
         # Initialize the dataloader
@@ -263,10 +278,10 @@ class test_DataLoading(ut.TestCase):
         # Build the model, and run it on IPU
         model = self.TestSimpleLightning(batch_size, node_feat_size, edge_feat_size, num_batch)
         plugins = IPUPlugin(training_opts=training_opts, inference_opts=inference_opts)
-        trainer = Trainer(logger=False, enable_checkpointing=False, max_epochs=2, plugins=plugins, num_sanity_val_steps=0)
+        trainer = Trainer(
+            logger=False, enable_checkpointing=False, max_epochs=2, plugins=plugins, num_sanity_val_steps=0
+        )
         trainer.fit(model=model, train_dataloaders=train_dataloader, val_dataloaders=val_dataloader)
-
-
 
     def test_poptorch_goli_deviceiterations_gradient_accumulation(self):
 
@@ -284,9 +299,9 @@ class test_DataLoading(ut.TestCase):
         opts = poptorch.Options()
         opts.deviceIterations(device_iterations)
         opts.Jit.traceModel(True)
-        training_opts=deepcopy(opts)
+        training_opts = deepcopy(opts)
         training_opts.Training.gradientAccumulation(gradient_accumulation)
-        inference_opts=deepcopy(opts)
+        inference_opts = deepcopy(opts)
 
         # Load the configuration file for the model
         CONFIG_FILE = "tests/config_test_ipu_dataloader.yaml"
@@ -306,11 +321,21 @@ class test_DataLoading(ut.TestCase):
 
         model_class, model_kwargs = load_architecture(cfg, in_dims=datamodule.in_dims)
         metrics = load_metrics(cfg)
-        predictor = self.TestPredictor(batch_size=batch_size, node_batch_size=node_factor*batch_size, edge_batch_size=edge_factor*batch_size, in_dims=datamodule.in_dims, model_class=model_class, model_kwargs=model_kwargs, metrics=metrics, **cfg["predictor"])
+        predictor = self.TestPredictor(
+            batch_size=batch_size,
+            node_batch_size=node_factor * batch_size,
+            edge_batch_size=edge_factor * batch_size,
+            in_dims=datamodule.in_dims,
+            model_class=model_class,
+            model_kwargs=model_kwargs,
+            metrics=metrics,
+            **cfg["predictor"],
+        )
         plugins = IPUPluginGoli(training_opts=training_opts, inference_opts=inference_opts)
-        trainer = Trainer(logger=False, enable_checkpointing=False, max_epochs=2, plugins=plugins, num_sanity_val_steps=0)
+        trainer = Trainer(
+            logger=False, enable_checkpointing=False, max_epochs=2, plugins=plugins, num_sanity_val_steps=0
+        )
         trainer.fit(model=predictor, datamodule=datamodule)
-
 
 
 if __name__ == "__main__":
