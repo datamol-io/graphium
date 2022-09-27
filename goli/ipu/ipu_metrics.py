c6c1397d5eb9233ea8c14743dffc991ae97a2747
Oleksandr Dymov <oleksandr@valencediscovery.com>
2022-09-27 13:30:14 +0000

    Added partial precisison an recall metrics

--- goli/ipu/ipu_metrics.py
+++ goli/ipu/ipu_metrics.py
@@ -1,7 +1,7 @@
 import torch
 from torch import Tensor
 from torch.nn import BCELoss, MSELoss, L1Loss
-from torchmetrics.functional import auroc, average_precision
+from torchmetrics.functional import auroc, average_precision, precision, accuracy, recall
 from torch._C import _infer_size
 
 from typing import Optional, Sequence
@@ -136,5 +136,183 @@ def auroc_ipu(
         max_fpr = max_fpr,
         sample_weights = sample_weights
     )
+
+    return score
+
+def average_precision_ipu(
+    preds: Tensor,
+    target: Tensor,
+    num_classes: Optional[int] = None,
+    pos_label: Optional[int] = None,
+    average: Optional[str] = "macro",
+    sample_weights: Optional[Sequence] = None,
+    ):
+    """
+    A modified version of the `torchmetrics.functional.average_precision` that can ignore NaNs
+    by giving them the same value for both `input` and `target`.
+    This allows it to work with compilation
+    and IPUs since it doesn't modify the tensor's shape.
+    """
+
+    target = target.clone()
+    preds = preds.clone()
+
+    # Replace the nan-targets in the preds/target tensors by 0
+    nan_targets = target.isnan()
+    preds[nan_targets] = 0.0
+    target[nan_targets] = 0.0
+
+    # Get the original weight matrix. If None, set all weights = 1
+    if sample_weights is None:
+        sample_weights = torch.ones(target.shape[0], dtype=preds.dtype, device=preds.device)
+    sample_weights[nan_targets] = 0.0
+
+    # Compute the loss, and rescale by the number of nan elements
+    score = average_precision (
+        preds = preds,
+        target = target.to(int),
+        num_classes = num_classes,
+        pos_label = pos_label,
+        average = average,
+        sample_weights = sample_weights)
+
+    return score
+
+def precision_ipu(
+    preds: Tensor,
+    target: Tensor,
+    average: Optional[str] = "micro",
+    mdmc_average: Optional[str] = None,
+    ignore_index: Optional[int] = None,
+    num_classes: Optional[int] = None,
+    threshold: float = 0.5,
+    top_k: Optional[int] = None,
+    multiclass: Optional[bool] = None,
+    ):
+    """
+    A modified version of the `torchmetrics.functional.precision` that can ignore NaNs
+    by giving them the same value for both `input` and `target`.
+    This allows it to work with compilation
+    and IPUs since it doesn't modify the tensor's shape.
+    """
+
+    target = target.clone()
+    preds = preds.clone()
+
+    nans = torch.isnan(target)
+    target[nans] = 1
+    preds[nans] = 0
+
+    # Compute the loss, and rescale by the number of nan elements
+    score = precision (
+        preds = preds,
+        target = target.to(int),
+        average = average,
+        mdmc_average = mdmc_average,
+        ignore_index = ignore_index,
+        num_classes = num_classes,
+        threshold = threshold,
+        top_k = top_k,
+        multiclass = multiclass)
+
+    return score
+
+def accuracy_ipu(
+    preds: Tensor,
+    target: Tensor,
+    average: Optional[str] = "micro",
+    mdmc_average: Optional[str] = "global",
+    threshold: float = 0.5,
+    top_k: Optional[int] = None,
+    subset_accuracy: bool = False,
+    num_classes: Optional[int] = None,
+    multiclass: Optional[bool] = None,
+    ignore_index: Optional[int] = None
+    ):
+    """
+    A modified version of the `torchmetrics.functional.precision` that can ignore NaNs
+    by giving them the same value for both `input` and `target`.
+    This allows it to work with compilation
+    and IPUs since it doesn't modify the tensor's shape.
+    """
+
+    target = target.clone()
+    preds = preds.clone()
+
+    nans = torch.isnan(target)
+    target[nans] = 1
+    preds[nans] = 0
+
+    # Replace the nan-targets in the preds/target tensors by 0
+    # nan_targets = target.isnan()
+    # preds[nan_targets] = 0.0
+    # target[nan_targets] = 0.0
+
+    # # Get the original weight matrix. If None, set all weights = 1
+    # if sample_weights is None:
+    #     sample_weights = torch.ones(target.shape[0], dtype=preds.dtype, device=preds.device)
+    # sample_weights[nan_targets] = 0.0
+
+    # Compute the loss, and rescale by the number of nan elements
+    score = accuracy (
+        preds = preds,
+        target = target.to(int),
+        average = average,
+        mdmc_average = mdmc_average,
+        threshold = threshold,
+        top_k = top_k,
+        subset_accuracy = subset_accuracy,
+        num_classes = num_classes,
+        multiclass = multiclass,
+        ignore_index = ignore_index)
+
+    return score
+
+def recall_ipu(
+    preds: Tensor,
+    target: Tensor,
+    average: Optional[str] = "micro",
+    mdmc_average: Optional[str] = None,
+    ignore_index: Optional[int] = None,
+    num_classes: Optional[int] = None,
+    threshold: float = 0.5,
+    top_k: Optional[int] = None,
+    multiclass: Optional[bool] = None
+    ):
+    """
+    A modified version of the `torchmetrics.functional.precision` that can ignore NaNs
+    by giving them the same value for both `input` and `target`.
+    This allows it to work with compilation
+    and IPUs since it doesn't modify the tensor's shape.
+    """
+
+    target = target.clone()
+    preds = preds.clone()
+
+    nans = torch.isnan(target)
+    target[nans] = 1
+    preds[nans] = 0
+
+    # Replace the nan-targets in the preds/target tensors by 0
+    # nan_targets = target.isnan()
+    # preds[nan_targets] = 0.0
+    # target[nan_targets] = 0.0
+
+    # # Get the original weight matrix. If None, set all weights = 1
+    # if sample_weights is None:
+    #     sample_weights = torch.ones(target.shape[0], dtype=preds.dtype, device=preds.device)
+    # sample_weights[nan_targets] = 0.0
+
+    # Compute the loss, and rescale by the number of nan elements
+    score = recall (
+        preds = preds,
+        target = target.to(int),
+        average = average,
+        mdmc_average = mdmc_average,
+        ignore_index = ignore_index,
+        num_classes = num_classes,
+        threshold = threshold,
+        top_k = top_k,
+        multiclass = multiclass)
 
     return score
