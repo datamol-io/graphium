96b681fe4cc4d20622f6dd4603bbee272fb92e3a
DomInvivo <dominique@valencediscovery.com>
2022-09-29 20:42:43 +0000

    Minor

--- expts/main_run_multitask.py
+++ expts/main_run_multitask.py
@@ -19,8 +19,8 @@ import wandb
 
 # Set up the working directory
 MAIN_DIR = dirname(dirname(abspath(goli.__file__)))
-CONFIG_FILE = "expts/configs/config_molPCQM4Mv2.yaml"
-# CONFIG_FILE = "expts/configs/config_ipu_allsizes.yaml"
+# CONFIG_FILE = "expts/configs/config_molPCQM4Mv2.yaml"
+CONFIG_FILE = "expts/configs/config_ipu_allsizes.yaml"
 # CONFIG_FILE = "expts/configs/config_ipu_reproduce.yaml"
 os.chdir(MAIN_DIR)
 
