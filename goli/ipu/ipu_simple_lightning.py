c02ebbec82504766e19f57c6b897a14657700a84
DomInvivo <dominique@valencediscovery.com>
2022-11-01 15:27:13 +0000

    More metrics + validation for ipu_simple_lightning

--- goli/ipu/ipu_simple_lightning.py
+++ goli/ipu/ipu_simple_lightning.py
@@ -59,7 +59,8 @@ class SimpleLightning(pl.LightningModule):
         prediction = self.model(x)
         preds = torch.argmax(prediction, dim=1)
         acc = torch.sum(preds == label).float() / len(label)
-        return acc
+        loss = torch.nn.functional.nll_loss(prediction, label)
+        return loss, acc
 
     # PopTorch doesn't currently support logging within steps. Use the Lightning
     # callback hooks instead.
@@ -67,7 +68,11 @@ class SimpleLightning(pl.LightningModule):
         self.log("StepLoss", outputs["loss"])
 
     def validation_epoch_end(self, outputs):
-        self.log("val_acc", torch.stack(outputs).mean(), prog_bar=True)
+        loss = [out[0] for out in outputs]
+        self.log("val_loss", torch.stack(loss).mean(), prog_bar=True)
+
+        acc = [out[1] for out in outputs]
+        self.log("val_acc", torch.stack(acc).mean(), prog_bar=True)
 
     def configure_optimizers(self):
         optimizer = mup.optim.Adam(self.parameters(), lr=0.01)
@@ -75,37 +80,48 @@ class SimpleLightning(pl.LightningModule):
 
 
 if __name__ == "__main__":
+
+    SEED = 42
+    torch.manual_seed(SEED)
+
     # Create the model as usual.
-    base = SimpleLightning(in_dim=1, hidden_dim=8, kernel_size=3, num_classes=10)
+    base = None # SimpleLightning(in_dim=1, hidden_dim=8, kernel_size=3, num_classes=10)
     model = SimpleLightning(in_dim=1, hidden_dim=32, kernel_size=3, num_classes=10)
     model = set_base_shapes(model, base, rescale_params=False)
 
+    torch.manual_seed(SEED)
     # Normal PyTorch dataset.
     train_set = torchvision.datasets.FashionMNIST(
         "out/FashionMNIST", train=True, download=True, transform=transforms.Compose([transforms.ToTensor()])
     )
+    val_set = torchvision.datasets.FashionMNIST(
+        "out/FashionMNIST", train=False, download=True, transform=transforms.Compose([transforms.ToTensor()])
+    )
 
     # Normal PyTorch dataloader.
     train_loader = torch.utils.data.DataLoader(train_set, batch_size=16, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(val_set, batch_size=16, shuffle=False)
 
     training_opts = poptorch.Options()
     training_opts.Jit.traceModel(True)
     inference_opts = poptorch.Options()
     inference_opts.Jit.traceModel(True)
+
+    # Set the seeds
+    torch.manual_seed(SEED)
+    training_opts.randomSeed(SEED)
+    inference_opts.randomSeed(SEED)
+
     plugins = pl.plugins.IPUPlugin(training_opts=training_opts, inference_opts=inference_opts)
-    # Run on IPU using IPUs=1. This will run on IPU but will not include any custom
-    # PopTorch Options. Changing IPUs to 1 to IPUs=N will replicate the graph N
-    # times. This can lead to issues with the DataLoader batching - the script
-    # ipu_options_and_dataloading.py shows how these can be avoided through the
-    # use of IPUOptions.
+
     trainer = pl.Trainer(
         logger=pl.loggers.WandbLogger(),
-        ipus=1,
+        # ipus=1, # Comment this line to run on CPU
         max_epochs=3,
         progress_bar_refresh_rate=20,
         log_every_n_steps=1,
-        plugins=plugins,
+        # plugins=plugins, # Comment this line to run on CPU
     )
 
     # When fit is called the model will be compiled for IPU and will run on the available IPU devices.
-    trainer.fit(model, train_loader)
+    trainer.fit(model, train_dataloaders=train_loader, val_dataloaders=val_loader)
