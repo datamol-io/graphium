420766c3226bc1c70dc8ddd9937a1e40de074f93
DomInvivo <dominique@valencediscovery.com>
2022-12-20 20:09:46 +0000

    Removed types_conversion

--- goli/ipu/ipu_dataloader.py
+++ goli/ipu/ipu_dataloader.py
@@ -139,7 +139,7 @@ class CombinedBatchingCollator:
                 dataset_max_edges_per_graph=self.dataset_max_edges_per_graph,
             )
 
-            local_batch["features"], local_batch["_types_conversion"] = transform(local_batch["features"])
+            local_batch["features"] = transform(local_batch["features"])
             all_batches.append(local_batch)
 
         out_batch = {}
@@ -157,10 +157,9 @@ class CombinedBatchingCollator:
 
         # TODO: Make this more robust, instead of hard-coding the keys
         out_batch["features"] = stacked_features
-        out_batch["_types_conversion"] = [this_batch["_types_conversion"] for this_batch in all_batches]
         out_batch["_batch_idx"] = torch.as_tensor(range(len(all_batches)), dtype=torch.int64).unsqueeze(-1)
         for key in all_batches[0].keys():
-            if key not in ("features", "labels", "_types_conversion"):
+            if key not in ("features", "labels"):
                 out_batch[key] = [this_batch[key] for this_batch in all_batches]
 
         return out_batch
@@ -379,26 +378,13 @@ class Pad(BaseTransform):
             [self.dataset_max_edges_per_graph], dtype=torch.int32
         )
 
-        # Convert integer types to smaller integers for faster transfer of data to IPUs
-        _types_conversion = {}
-        int_types = [torch.int16, torch.int32, torch.int64]
-        for key, val in new_batch.items():
-            if isinstance(val, torch.Tensor) and (val.dtype in int_types[1:]):
-                for this_int_type in int_types:
-                    if (val.max() <= torch.iinfo(this_int_type).max) and (
-                        val.min() >= torch.iinfo(this_int_type).min
-                    ):
-                        _types_conversion[key] = val.dtype
-                        new_batch[key] = val.to(this_int_type)
-                        break
-
         # Convert float types to smaller floats for faster transfer of data to IPUs
         float_types = [torch.float16, torch.float32, torch.float64]
         for key, val in new_batch.items():
             if isinstance(val, torch.Tensor) and (val.dtype in float_types[1:]):
                 new_batch[key] = val.to(torch.float16)
 
-        return new_batch, _types_conversion
+        return new_batch
 
     def __repr__(self) -> str:
         s = f"{self.__class__.__name__}("
