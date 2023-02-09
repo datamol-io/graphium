47da8b86437170dc1b84825e1a50b5ddb1a56abd
Callum McLean <callumm@graphcore.ai>
2023-02-09 16:05:27 +0000

    Respond to review #1

--- goli/ipu/ipu_utils.py
+++ goli/ipu/ipu_utils.py
@@ -34,7 +34,7 @@ def load_ipu_options(
     seed: Optional[int] = None,
     model_name: Optional[str] = None,
     gradient_accumulation: Optional[int] = None,
-    ipu_inference_file: Optional[str] = None,
+    ipu_inference_overrides: Optional[str] = None,
 ) -> Tuple["poptorch.Options", "poptorch.Options"]:
     """
     Load the IPU options from the config file.
@@ -70,7 +70,8 @@ def load_ipu_options(
 
         seed: random seed for the IPU
         model_name: Name of the model, to be used for ipu profiling
-        ipu_inference_file: file path containing IPU configurations for inference
+        ipu_inference_overrides: optional file path containing IPU configuration overrides for inference.
+            If this file is provided, options in this file override those in `ipu_file` for inference.
 
     Returns:
 
@@ -78,7 +79,7 @@ def load_ipu_options(
 
         inference_opts: IPU options for inference.
             It differs from the `training_opts` by enforcing `gradientAccumulation` to 1,
-            and applying any overrides in `ipu_inference_file` if provided
+            and applying any overrides in `ipu_inference_overrides` if provided
 
     """
 
@@ -106,7 +107,7 @@ def load_ipu_options(
     inference_opts.Training.gradientAccumulation(1)
     if model_name is not None:
         inference_opts.modelName(f"{model_name}_inference")
-    if ipu_inference_file is not None:
-        inference_opts.loadFromFile(ipu_inference_file)
+    if ipu_inference_overrides is not None:
+        inference_opts.loadFromFile(ipu_inference_overrides)
 
     return training_opts, inference_opts
