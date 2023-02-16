d438adf7daf099de68dcf58153ac2c067da18fac
DomInvivo <dominique@invivoai.com>
2023-02-16 10:39:19 -0500

    Fixed unit-tests

--- goli/nn/pyg_layers/gps_pyg.py
+++ goli/nn/pyg_layers/gps_pyg.py
@@ -179,8 +179,8 @@ class GPSLayerPyg(BaseGraphModule):
 
         # Add the droppath to the output of the MLP
         batch_size = None if h.device.type != "ipu" else batch.graph_is_true.shape[0]
-        if self.droppath_ffn is not None:
-            h = self.droppath_ffn(h, batch, batch_size)
+        if self.droppath_layer is not None:
+            h = self.droppath_layer(h, batch.batch, batch_size)
 
         batch_out.h = h
 
