ac04a01c952a671b18621af846e7047cfeb85c46
shenyangHuang <shenyang.huang@mail.mcgill.ca>
2023-03-15 17:54:23 +0000

    adding linting

--- goli/nn/architectures/global_architectures.py
+++ goli/nn/architectures/global_architectures.py
@@ -827,7 +827,11 @@ class FeedForwardGraphBase(FeedForwardNN):
         raise NotImplementedError("Virtual method must be overwritten by child class")
 
     def _virtual_node_forward(
+<<<<<<< HEAD
         self, g: Union[DGLGraph, Data], h: torch.Tensor, vn_h: torch.Tensor, step_idx: int, e: torch.Tensor
+=======
+        self, g: Union[DGLGraph, Data], h: torch.Tensor, vn_h: torch.Tensor, step_idx: int
+>>>>>>> minor update, all tested fine
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         r"""
         Apply the *i-th* virtual node layer, where *i* is the index given by `step_idx`.
@@ -985,7 +989,13 @@ class FeedForwardGraphBase(FeedForwardNN):
         kwargs.update(new_kwargs)
         return deepcopy(kwargs)
 
+<<<<<<< HEAD
     def make_mup_base_kwargs(self, divide_factor: float = 2.0, factor_in_dim: bool = False) -> Dict[str, Any]:
+=======
+    def make_mup_base_kwargs(
+        self, divide_factor: float = 2.0, factor_in_dim: bool = False, factor_in_dim_edges: bool = False
+    ) -> Dict[str, Any]:
+>>>>>>> minor update, all tested fine
         """
         Create a 'base' model to be used by the `mup` or `muTransfer` scaling of the model.
         The base model is usually identical to the regular model, but with the
