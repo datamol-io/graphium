b39076a5b84b40adf65af0d5f712851ba2d2e4b1
DomInvivo <dominique@valencediscovery.com>
2023-04-18 23:17:00 +0000

    Fixed the GPS configs. Added `mask_nans` to the conformer positions

--- expts/main_run_multitask.py
+++ expts/main_run_multitask.py
@@ -29,7 +29,8 @@ import wandb
 MAIN_DIR = dirname(dirname(abspath(goli.__file__)))
 
 # CONFIG_FILE = "expts/configs/config_mpnn_10M_b3lyp.yaml"
-CONFIG_FILE = "expts/configs/config_mpnn_10M_pcqm4m.yaml"
+# CONFIG_FILE = "expts/configs/config_mpnn_10M_pcqm4m.yaml"
+CONFIG_FILE = "expts/configs/config_gps_10M_pcqm4m.yaml"
 os.chdir(MAIN_DIR)
 
 
