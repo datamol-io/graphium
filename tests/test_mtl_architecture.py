0ce4fc15a2b4eab420b7fb8bc5c6f8adad4c5083
pupuis <luismueller2309@gmail.com>
2023-04-20 09:47:24 +0200

    Minor fix to nodepair test

--- tests/test_mtl_architecture.py
+++ tests/test_mtl_architecture.py
@@ -150,18 +150,18 @@ class test_GraphOutputNN(ut.TestCase):
         x = torch.concat([g_1, g_2, g_3])
 
         expected_result = [
-            # Graph 1, 4 nodes
+            # Graph 1
             [2, 4, 4, 2, 2, 4],
             [2, 4, 5, 2, 2, 3],
             [3, 3, 4, 1, 3, 4],
             [0, 2, 1, 0, 0, 1],
             [1, 1, 0, 1, 1, 0],
             [1, 1, 1, 1, 1, 1],
-            # Graph 2, 3 nodes
+            # Graph 2
             [3, 5, 1, 3, 3, 1],
             [4, 4, 0, 2, 4, 0],
             [1, 1, 1, 1, 1, 1],
-            # Graph 3, first 4 nodes
+            # Graph 3
             [3, 5, 1, 3, 3, 1],
             [4, 4, 0, 2, 4, 0],
             [5, 6, 2, 1, 2, 2],
