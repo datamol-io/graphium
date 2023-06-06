34c378915fcbc12d4f6162fd32aa79f14741815f
DomInvivo <dominique@invivoai.com>
2023-06-06 18:26:08 -0400

    black linting

--- tests/test_data_utils.py
+++ tests/test_data_utils.py
@@ -3,16 +3,20 @@ import unittest as ut
 import graphium
 import tempfile
 
+
 class TestDataUtils(ut.TestCase):
-    def test_list_datasets(self, ):
+    def test_list_datasets(
+        self,
+    ):
         datasets = graphium.data.utils.list_graphium_datasets()
         assert isinstance(datasets, set)
         assert len(datasets) > 0
 
-
     def test_download_datasets(self):
         dataset_dir = tempfile.TemporaryDirectory()
-        data_path = graphium.data.utils.download_graphium_dataset("graphium-zinc-micro", output_path=dataset_dir.name)
+        data_path = graphium.data.utils.download_graphium_dataset(
+            "graphium-zinc-micro", output_path=dataset_dir.name
+        )
 
         fpath = graphium.utils.fs.join(data_path, "ZINC-micro.csv")
         df = pd.read_csv(fpath)
