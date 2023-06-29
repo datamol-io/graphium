some fixes: keys() and use normalization flag on val and test for baseline configs  (#367)
* minor fixes to test the toymix datasets

* add normalize_val_test flag to baseline configs

* attempting to fix tests and make changes to gey keys so that it works on both object and dict

* fix a few other tests when run locally

* changes on the keys and keys()

* baseline config changes

* minor changes after testing

graphium/data/collate.py
@@ -11,6 +11,7 @@ from torch_geometric.data import Data, Batch
 from graphium.features import GraphDict, to_dense_array
 from graphium.utils.packing import fast_packing, get_pack_sizes, node_to_pack_indices_mask
 from loguru import logger
+from graphium.data.utils import get_keys
 
 
 def graphium_collate_fn(
@@ -130,7 +131,7 @@ def collage_pyg_graph(pyg_graphs: Iterable[Union[Data, Dict]], batch_size_per_pa
 
     pyg_batch = []
     for pyg_graph in pyg_graphs:
-        for pyg_key in pyg_graph.keys:
+        for pyg_key in get_keys(pyg_graph):
             tensor = pyg_graph[pyg_key]
 
             # Convert numpy/scipy to Pytorch
@@ -198,7 +199,7 @@ def collate_pyg_graph_labels(pyg_labels: List[Data]):
     """
     pyg_batch = []
     for pyg_label in pyg_labels:
-        for pyg_key in set(pyg_label.keys) - set(["x", "edge_index"]):
+        for pyg_key in set(get_keys(pyg_label)) - set(["x", "edge_index"]):
             tensor = pyg_label[pyg_key]
             # Convert numpy/scipy to Pytorch
             if isinstance(tensor, (ndarray, spmatrix)):
@@ -255,14 +256,14 @@ def collate_labels(
                     labels_size_dict[task] = labels_size_dict[task][1:]
                 elif not task.startswith("graph_"):
                     labels_size_dict[task] = [1]
-
-            empty_task_labels = set(labels_size_dict.keys()) - set(this_label.keys)
+            label_keys_set = set(get_keys(this_label))
+            empty_task_labels = set(labels_size_dict.keys()) - label_keys_set
             for task in empty_task_labels:
                 labels_size_dict[task] = get_expected_label_size(this_label, task, labels_size_dict[task])
                 dtype = labels_dtype_dict[task]
                 this_label[task] = torch.full([*labels_size_dict[task]], torch.nan, dtype=dtype)
 
-            for task in set(this_label.keys) - set(["x", "edge_index"]) - empty_task_labels:
+            for task in label_keys_set - set(["x", "edge_index"]) - empty_task_labels:
                 labels_size_dict[task] = get_expected_label_size(this_label, task, labels_size_dict[task])
 
                 if not isinstance(this_label[task], (torch.Tensor)):