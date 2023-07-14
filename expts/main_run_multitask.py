ff7a8a200aef9cef9747779b558059768e1278cc
Luis Müller <luismueller2309@gmail.com>
2023-07-14 19:11:04 +0200

    Final adjustments, ToyMix and LargeMix run fine on GPU

--- expts/main_run_multitask.py
+++ expts/main_run_multitask.py
@@ -36,15 +36,14 @@ MAIN_DIR = dirname(dirname(abspath(graphium.__file__)))
 # CONFIG_FILE = "expts/configs/config_mpnn_10M_pcqm4m.yaml"
 # CONFIG_FILE = "expts/neurips2023_configs/config_debug.yaml"
 # CONFIG_FILE = "expts/neurips2023_configs/config_large_mpnn.yaml"
-# CONFIG_FILE = "expts/neurips2023_configs/debug/config_large_gcn_debug.yaml"
+CONFIG_FILE = "expts/neurips2023_configs/debug/config_small_gcn_debug.yaml"
 # CONFIG_FILE = "expts/neurips2023_configs/config_large_gin.yaml"
-# CONFIG_FILE = "expts/neurips2023_configs/config_large_gcn.yaml"
 # CONFIG_FILE = "expts/neurips2023_configs/config_large_gine.yaml"
 # CONFIG_FILE = "expts/neurips2023_configs/config_small_gcn.yaml"
 # CONFIG_FILE = "expts/neurips2023_configs/config_small_gin.yaml"
 # CONFIG_FILE = "expts/neurips2023_configs/config_small_gine.yaml"
-CONFIG_FILE = "expts/neurips2023_configs/config_small_gcn_luis.yaml"
-# CONFIG_FILE = "expts/neurips2023_configs/config_pcqm4m_gcn.yaml"
+# CONFIG_FILE = "expts/neurips2023_configs/config_small_gcn_gpu.yaml"
+# CONFIG_FILE = "expts/neurips2023_configs/config_large_gcn_gpu.yaml"
 os.chdir(MAIN_DIR)
 
 
@@ -55,7 +54,7 @@ def main(cfg: dict, run_name: str = "main", add_date_time: bool = True) -> None:
     if add_date_time:
         date_time_suffix = datetime.now().strftime("%d.%m.%Y_%H.%M.%S")
 
-    wandb.init(entity="lmueller", project=cfg["constants"]["name"], config=cfg)
+    wandb.init(entity="multitask-gnn", project=cfg["constants"]["name"], config=cfg)
 
     # Initialize the accelerator
     cfg, accelerator_type = load_accelerator(cfg)
@@ -115,4 +114,5 @@ if __name__ == "__main__":
     if args.config is not None:
         CONFIG_FILE = args.config
     cfg = load_yaml_config(CONFIG_FILE, MAIN_DIR, unknown_args)
+
     main(cfg)
