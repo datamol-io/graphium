b57819f394791bb66bb8fccc1bb6a3996d321609
Sam Maddrell-Mander <samuelm@graphcore.ai>
2023-08-15 13:55:01 +0000

    removing extra files, and all using override

--- tests/test_ipu_training.py
+++ tests/test_ipu_training.py
@@ -33,7 +33,34 @@ def test_cli(acc_type) -> None:
     """
     with hydra.initialize(version_base=None, config_path="../expts/hydra-configs"):
         # config is relative to a module
-        cfg = hydra.compose(config_name="test", overrides=[f"accelerator={acc_type}"])
+        cfg = hydra.compose(
+            config_name="main",
+            overrides=[
+                f"accelerator={acc_type}",
+                "tasks=toymix",
+                "training=toymix",
+                # Reducing number of parameters in the toymix architecture
+                "architecture=toymix",
+                "architecture.pe_encoders.encoders.la_pos.hidden_dim=16",
+                "architecture.pe_encoders.encoders.la_pos.num_layers=1",
+                "architecture.pe_encoders.encoders.rw_pos.hidden_dim=16",
+                "architecture.pe_encoders.encoders.rw_pos.num_layers=1",
+                "architecture.pre_nn.hidden_dims=32",
+                "architecture.pre_nn.depth=1",
+                "architecture.pre_nn.out_dim=16",
+                "architecture.gnn.in_dim=16",
+                "architecture.gnn.out_dim=16",
+                "architecture.gnn.depth=2",
+                "architecture.task_heads.qm9.depth=1",
+                "architecture.task_heads.tox21.depth=1",
+                "architecture.task_heads.zinc.depth=1",
+                # Set the number of epochs
+                "constants.max_epochs=2",
+                "+datamodule.args.task_specific_args.qm9.sample_size=1000",
+                "+datamodule.args.task_specific_args.tox21.sample_size=1000",
+                "+datamodule.args.task_specific_args.zinc.sample_size=1000",
+            ],
+        )
         if acc_type == "ipu":
             cfg["accelerator"]["ipu_config"].append("useIpuModel(True)")
             cfg["accelerator"]["ipu_inference_config"].append("useIpuModel(True)")
