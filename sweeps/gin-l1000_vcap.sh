af948e173451e17a6e8bafe8a5f5e4a14300c9ea
WenkelF <frederik.wenkel@cn-g019.server.mila.quebec>
2023-08-23 15:17:26 -0400

    scripts for sbatch

---
+++ sweeps/gin-l1000_vcap.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+## Name of your SLURM job
+#SBATCH --job-name=sweep
+
+## Files for logs: here we redirect stoout and sterr to the same file
+#SBATCH --output=outputs/sweep.out
+#SBATCH --error=outputs/error_sweep.out
+#SBATCH --open-mode=append
+
+## Time limit for the job
+#SBATCH --time=120:00:00
+
+## Partition to use,
+#SBATCH --partition=a1001
+
+set -e
+
+source /home/frederik_valencediscovery_com/.bashrc
+cd /home/frederik_valencediscovery_com/projects/graphium_expts
+source activate graphium_dev
+
+wandb agent multitask-gnn/neurips2023-large-single-dataset/kmtjw7up
