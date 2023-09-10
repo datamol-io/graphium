5f38ce5112d9df0bf25c96f6569c2501d4687a67
hussein-mohamu.jama <hussein-mohamu.jama@cn-g025.server.mila.quebec>
2023-09-10 17:22:41 -0400

    created different job files for depths

--- graphium_train_job_depth_4.sh
+++ graphium_train_job_depth_4.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --job-name=graphium_training
-#SBATCH --output=graphium_train_output.txt
-#SBATCH --error=graphium_train_error.txt
+#SBATCH --job-name=graphium_training_depth_4
+#SBATCH --output=graphium_train_output_depth_4.txt
+#SBATCH --error=graphium_train_error_depth_4.txt
 #SBATCH --ntasks=1
 #SBATCH --time=48:00:00   
-#SBATCH --mem=32Gb
-#SBATCH --partition=unkillable
+#SBATCH --mem=48Gb
+#SBATCH --partition=main
 #SBATCH --gres=gpu:a100l:1
-#SBATCH --cpus-per-task=6
+#SBATCH --cpus-per-task=8
 
 # Activate the conda environment
 source /home/mila/h/hussein-mohamu.jama/miniconda3/etc/profile.d/conda.sh
@@ -15,4 +15,3 @@ conda activate graphium
 
 # Execute the training command
 graphium-train model=gated_gcn accelerator=gpu +hparam_search=optuna
-
