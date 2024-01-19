de5a23b16fb3f3330aaaf94e5eff0d0109fca6a2
DomInvivo <dominique@invivoai.com>
2024-01-19 09:28:19 -0500

    A few more re-ordering tests, not complete

--- tests/test_canonical_reordering.py
+++ tests/test_canonical_reordering.py
@@ -16,15 +16,19 @@ Refer to the LICENSE file for the full terms and conditions.
 Unit tests for the implementation of canonical reordering of molecules in `dataset.py` and `datamodule.py`
 """
 
+from typing import Literal
+
 import unittest as ut
 from copy import deepcopy
 import torch.nn as nn
 import torch
-
-from torch_geometric.data import Batch, Data
+import numpy as np
+import pandas as pd
+import datamol as dm
 
 from graphium.utils.enums import TaskLevel as TL
 from graphium.data.multilevel_utils import get_canonical_ranks_pair
+from graphium.data.dataset import SingleTaskDataset
 
 
 class test_task_level(ut.TestCase):
@@ -100,6 +104,89 @@ class test_node_reordering(ut.TestCase):
                 None,
             ],
         )
+
+    def test_labels_reordering(self):
+
+        labels = list(torch.randn(6, 3))
+        all_canonical_ranks = [[1, 2, 3], [2, 3, 1], [3, 2, 1], [1, 2, 3], None, [2, 3, 1]]
+        unique_ids_inv = [0, 0, 0, 0, 0, 0]
+        task_level = TL.NODE
+        all_task_levels = [task_level for _ in range(len(all_canonical_ranks))]
+        canonical_ranks_pair = get_canonical_ranks_pair(all_canonical_ranks, all_task_levels, unique_ids_inv)
+        reordered_labels = SingleTaskDataset.reorder_labels(labels, canonical_ranks_pair, task_level)
+
+        self.assertListEqual(labels[0].tolist(), reordered_labels[0].tolist())
+        self.assertListEqual(labels[1].tolist(), reordered_labels[1][[1, 2, 0]].tolist())
+        self.assertListEqual(labels[2].tolist(), reordered_labels[2][[2, 1, 0]].tolist())
+        self.assertListEqual(labels[3].tolist(), reordered_labels[3].tolist())
+        self.assertListEqual(labels[4].tolist(), reordered_labels[4].tolist())
+        self.assertListEqual(labels[5].tolist(), reordered_labels[5][[1, 2, 0]].tolist())
+
+    def test_datamodule_reordering(self):
+
+        # Make a dummy molecular dataset
+        smiles = ["C", "CC", "CCC", "CCCC", "CCCCC", "CCCCCC", "CCCCCCC"]
+        labels = [np.random.randn(len(s)) for s in smiles]
+        mols = [dm.to_mol(s) for s in smiles]
+        ordered_smiles = [dm.to_smiles(m, canonical=False, with_atom_indices=True) for m in mols]
+
+        # Reorder the molecules randomly
+        np.random.seed(42)
+        reordering_idx = [np.random.permutation(len(s)) for s in smiles]
+        reordered_mols = [reorder_atoms_to_desired(m, idx.tolist()) for m, idx in zip(mols, reordering_idx)]
+        reordered_smiles = [dm.to_smiles(m, canonical=False, with_atom_indices=True) for m in reordered_mols]
+
+
+        pass
+
+
+def ordered_carbon_smiles_generator(num_carbons, order: Literal["increasing", "random"]) -> str:
+    """Generate a list of smiles with a specified number of carbons in the molecule.
+
+    Args:
+        num_carbons: The number of carbons in the molecule.
+        order: The order of the carbons in the molecule. Either "increasing" or "random".
+
+    Returns:
+        smiles: A list of smiles with the specified number of carbons in the molecule.
+    """
+    if num_carbons == 1:
+        return "[CH4:0]"
+    elif num_carbons == 2:
+        return "[CH3:0][CH3:1]"
+    elif num_carbons > 2:
+
+        if order == "increasing":
+            return "".join([f"[CH3:{i}]" for i in range(num_carbons)])
+        elif order == "random":
+            idx = np.random.permutation(num_carbons)
+            return "".join([f"[CH3:{idx[i]}]" for i in range(num_carbons)])
+        else:
+            raise ValueError(f"Unknown order: {order}")
+
+
+
+
+from typing import List, Optional, Iterable
+from rdkit.Chem import Mol, rdmolops
+def reorder_atoms_to_desired(
+    mol: Mol,
+    desired_order: Iterable[int],
+) -> Optional[Mol]:
+    """Reorder the atoms in a mol. It ensures a single atom order for the same molecule,
+    regardless of its original representation.
+
+    Args:
+        mol: a molecule.
+        desired_order: The desired order of the atoms.
+
+    Returns:
+        mol: a molecule.
+    """
+    if mol.GetNumAtoms() == 0:
+        return mol
+
+    return rdmolops.RenumberAtoms(mol, list(desired_order))
 
 
 if __name__ == "__main__":
