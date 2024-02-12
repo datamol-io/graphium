7283712928f940b2814004f0c7258a53e73b0b62
DomInvivo <dominique@invivoai.com>
2024-02-12 14:56:46 -0500

    Minor bug fix in ensemble_layers

--- graphium/nn/ensemble_layers.py
+++ graphium/nn/ensemble_layers.py
@@ -455,4 +455,5 @@ class EnsembleMLP(MLP):
         Controls how the class is printed
         """
         rep = super().__repr__()
-        rep = rep[:-1] + f", num_ensemble={self.layers[0].linear.weight.shape[0]})"
+        rep = rep[:-1] + f", num_ensemble={self.fully_connected[0].linear.weight.shape[0]})"
+        return rep
