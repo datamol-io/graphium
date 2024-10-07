ec127fb68293ed9d0a0f5739c60716ee14bfef97
Andrew Quirke <andrewquirke99@gmail.com>
2024-10-07 15:55:32 -0400

    updating install script

--- scripts/post_install.py
+++ scripts/post_install.py
@@ -2,5 +2,5 @@ import subprocess
 import sys
 
 # Use the current Python interpreter to run pip
-subprocess.check_call(['ls', ''])
+subprocess.check_call(['ls'])
 # sys.executable, '-m', 'pip', 'install', package_name
